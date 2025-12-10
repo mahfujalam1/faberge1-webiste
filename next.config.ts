2fc2acb818fef27602d942f6c6c37d0844ef65d2
mahfujalam1 <mahfujahmad424@gmail.com>
2025-12-10 17:41:36 +0600

    nextjs image hostname resloved

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     // Allow images from all domains
-    domains: ['10.10.20.16'],
+    domains: ['10.10.20.16', 'x91h36px-5137.inc1.devtunnels.ms'],
     deviceSizes: [320, 420, 768, 1024, 1200, 1600, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 128, 256, 384, 512],
   },
