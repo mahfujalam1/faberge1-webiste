081171e12d27b03bb4055ed5e9c7cf93319345f3
mahfujalam1 <mahfujahmad424@gmail.com>
2025-12-30 18:16:35 +0600

    everything is fine

--- src/components/ForgotPassword/UpdatePassword.tsx
+++ src/components/ForgotPassword/UpdatePassword.tsx
@@ -42,7 +42,7 @@ export function StepThreePassword({ onComplete, isLoading }: StepThreePasswordPr
                         onClick={() => setShowNewPassword(!showNewPassword)}
                         className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500 hover:text-gray-700"
                     >
-                        {showNewPassword ? <EyeOff /> : <Eye />}
+                        {showNewPassword ? <Eye /> : <EyeOff />}
                     </button>
                 </div>
             </div>
--- src/components/Register/Password.tsx
+++ src/components/Register/Password.tsx
@@ -97,7 +97,7 @@ export default function StepTwoPassword({
                             onClick={() => setShowPassword(!showPassword)}
                             className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
                         >
-                            {showPassword ? <EyeOff className="h-5 w-5" /> : <Eye className="h-5 w-5" />}
+                            {showPassword ? <Eye className="h-5 w-5" /> :  <EyeOff className="h-5 w-5" />}
                         </button>
                     </div>
                 </div>
