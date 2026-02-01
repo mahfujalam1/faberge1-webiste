0e9656b743f57a5336941be6afe80e2c61801f08
mahfujalam1 <mahfujahmad424@gmail.com>
2026-02-01 10:28:05 +0600

    switch origin to mahfuj git

--- src/app/(user)/my-bookings/page.tsx
+++ src/app/(user)/my-bookings/page.tsx
@@ -12,8 +12,6 @@ export default function AllBookings() {
     const [tab, setTab] = useState<"" | "booked" | "completed">("");
     const [filterType, setFilterType] = useState<"" | "upcoming" | "completed">("");
 
-
-
     const [currentPage, setCurrentPage] = useState(1);
     const limit = 4;
 
