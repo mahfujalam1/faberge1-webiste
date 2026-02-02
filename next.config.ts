82ebebe6e1c892001ef7b4a0441a009bcb428795
mahfujalam1 <mahfujahmad424@gmail.com>
2026-02-02 15:02:51 +0600

    error solved

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,23 @@
 const nextConfig = {
   images: {
-    domains: ['98.93.16.125', 'x91h36px-5137.inc1.devtunnels.ms', "api.inhomebeautyservices.com", '10.10.20.16', 'https://x91h36px-5137.inc1.devtunnels.ms'],
-    deviceSizes: [320, 420, 768, 1024, 1200, 1600, 1920, 2048],
-    imageSizes: [16, 32, 48, 64, 128, 256, 384, 512],
-  },
-  rules: {
-    'react/no-unescaped-entities': 'off',
-    '@next/next/no-page-custom-font': 'off',
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '98.93.16.125',
+      },
+      {
+        protocol: 'https',
+        hostname: 'x91h36px-5137.inc1.devtunnels.ms',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.inhomebeautyservices.com',
+      },
+      {
+        protocol: 'http',
+        hostname: '10.10.20.16',
+      },
+    ],
   },
 };
 
