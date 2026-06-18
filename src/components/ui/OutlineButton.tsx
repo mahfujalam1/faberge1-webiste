77bf882d7f7b25f5502372415b380361ce119865
Ahsan-Mahfuz <ahsanmahfuz0@gmail.com>
2026-06-18 10:55:38 +0600

    done

--- src/components/ui/OutlineButton.tsx
+++ src/components/ui/OutlineButton.tsx
@@ -10,7 +10,7 @@ export const OutlineButton = ({ name, className }: OutlineButtonProps) => {
     return (
         <Button
             variant="outline"
-            className={cn("btn-outline text-xl cursor-pointer md:px-20 px-10 md:py-8 py-5 rounded-lg", className)}
+            className={cn("btn-outline text-xl cursor-pointer w-[250px]  md:px-20 px-10 md:py-8 py-5 rounded-lg", className)}
         >
             {name}
         </Button>
