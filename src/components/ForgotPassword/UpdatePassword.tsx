574ada9107cd82a73b87668ccff011849552cdab
Ahsan-Mahfuz <ahsanmahfuz0@gmail.com>
2026-06-18 09:22:05 +0600

    done

--- src/components/ForgotPassword/UpdatePassword.tsx
+++ src/components/ForgotPassword/UpdatePassword.tsx
@@ -63,7 +63,7 @@ export function StepThreePassword({ onComplete, isLoading }: StepThreePasswordPr
                         onClick={() => setShowConfirmPassword(!showConfirmPassword)}
                         className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500 hover:text-gray-700"
                     >
-                        {showConfirmPassword ? <EyeOff /> : <Eye />}
+                        {showConfirmPassword ? <Eye /> : <EyeOff />}
                     </button>
                 </div>
             </div>
